4d7c6ba7bf16b005a7a1afea0203ad174dc98875
Bethany Armitage <bethgrace5@gmail.com>
2016-08-06 18:58:18 -0700

    empty data view

---
+++ webapp/app/scripts/controllers/data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name techftw.controller:DataCtrl
+ * @description
+ * # DataCtrl
+ * Controller of the techftw
+ */
+angular.module('techftw')
+  .controller('DataCtrl', function ($scope, $rootScope, $http) {
+
+  });
